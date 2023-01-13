f82143506ecc26aeecb8990a967dd1058c1922a6
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-01-13 10:38:43 -0800

    Remove white space

--- app/controllers/doctor_patients_controller.rb
+++ app/controllers/doctor_patients_controller.rb
@@ -2,7 +2,6 @@ class DoctorPatientsController < ApplicationController
 
   def destroy
     @doctor = Doctor.find(params[:doctor_id])
-
     DoctorPatient.find_by(doctor_id: params[:doctor_id], patient_id: params[:patient_id]).destroy
     redirect_to doctor_path(@doctor)
   end
