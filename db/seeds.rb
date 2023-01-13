6c689fe6744c1053f97f58fa60d261e6a5b005f6
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-01-13 10:12:04 -0800

    Add destroy alls in seed in case reseed is needed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+DoctorPatient.destroy_all
+Doctor.destroy_all
+Patient.destroy_all
+Hospital.destroy_all
 
 @hospital = Hospital.create!(name: "Sacred Heart")
 
