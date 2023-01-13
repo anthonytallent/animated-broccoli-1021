763c6eced56eafe5c81ae253bc5e0323b4a9e624
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-01-13 10:02:10 -0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@hospital = Hospital.create!(name: "Sacred Heart")
+
+@doctor_1 = Doctor.create!(name: "Stan", specialty: "Cardiology", university: "U of R", hospital_id: @hospital.id)
+@doctor_2 = Doctor.create!(name: "Jeff", specialty: "Cardiology", university: "U of R", hospital_id: @hospital.id)
+
+@patient_1 = Patient.create!(name: "Steve", age: 42)
+@patient_2 = Patient.create!(name: "Justin", age: 28)
+@patient_3 = Patient.create!(name: "Joey", age: 15)
+@patient_4 = Patient.create!(name: "Brian", age: 22)
+
+DoctorPatient.create!(doctor_id: @doctor_1.id, patient_id: @patient_1.id)
+DoctorPatient.create!(doctor_id: @doctor_1.id, patient_id: @patient_2.id)
+DoctorPatient.create!(doctor_id: @doctor_2.id, patient_id: @patient_1.id)
+DoctorPatient.create!(doctor_id: @doctor_2.id, patient_id: @patient_2.id)
+DoctorPatient.create!(doctor_id: @doctor_2.id, patient_id: @patient_3.id)
+DoctorPatient.create!(doctor_id: @doctor_2.id, patient_id: @patient_4.id)
