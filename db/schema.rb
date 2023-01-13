9957a3977bc1a9a74e22396fc26289625cf0b75a
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-01-13 09:26:03 -0800

    add age column to patients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_13_163433) do
+ActiveRecord::Schema.define(version: 2023_01_13_172101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2023_01_13_163433) do
 
   create_table "patients", force: :cascade do |t|
     t.string "name"
+    t.integer "age"
   end
 
   add_foreign_key "doctor_patients", "doctors"
